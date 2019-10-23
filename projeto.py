fa265f00590f51049385066d14f1c6686ce83b87
Gustavo Moretto <gustavomtt@gmail.com>
2019-10-23 20:48:07 -0300

    parte 3

--- projeto.py
+++ projeto.py
@@ -3,6 +3,7 @@ from aluno import Aluno
 class Main:
     aluno = Aluno('101','Francis','10')
     aluno1= Aluno('102','Gustavo','8')
+    aluno2= Aluno('103','Filipe','9')
 
     arquivo = open('teste.csv', 'w') # Abra o arquivo (leitura)
     try:
@@ -10,9 +11,13 @@ class Main:
         writer.writerow(('Cod','Nome','Nota'))
         writer.writerow((aluno.codigo,aluno.nome,aluno.nota))
         writer.writerow((aluno1.codigo,aluno1.nome,aluno1.nota))
+        writer.writerow((aluno2.codigo,aluno2.nome,aluno2.nota))
     finally:
         arquivo.close()
     print(open('teste.csv','rt').read())
     conteudo = open('teste.csv','rt').read()
 
     print(conteudo)
+
+
+    transa1 = Transacao()
--- transacao.py
+++ transacao.py
@@ -1,1 +1,9 @@
 class Transacao:
+    def __init__(self, sessao, nro, arquivo)
+        # Gerar um numero de sessão aleatório
+        # Gerar um código aleatório
+        # Abrir um arquivo com o nome aleatorio (Talvez sessao + codigo)
+
+        self.sessao=sessao
+        self.nro=nro
+        self.arquivo=arquivo
